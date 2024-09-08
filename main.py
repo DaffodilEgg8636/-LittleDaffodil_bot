4549500b502623e97c199538a3b16da4b7e129e7
DaffodilEgg8636 <alexanderbodnar8@gmail.com>
2024-09-08 17:06:07 +0200

    updated handler

--- main.py
+++ main.py
@@ -26,5 +26,10 @@ I am here to echo your kind words back to you. Just say anything nice and I'll s
 def echo_message(message):
     bot.reply_to(message, message.text)
 
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+
 
 bot.infinity_polling()
